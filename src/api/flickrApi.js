ee481b55f01151be15cbd3a3ff46daf38f821f13
Pro <pro@Pro.local>
2020-07-04 10:54:13 +0200

    remove import apikey statement

--- src/api/flickrApi.js
+++ src/api/flickrApi.js
@@ -1,6 +1,5 @@
-import { apiKey } from "config";
-
 export default function FlickrApi(search) {
+  const apiKey = process.env.REACT_APP_FLICKR_API_KEY;
   return fetch(
     `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&text=${search}&sort=relevance&per_page=24&format=json&nojsoncallback=1`
   )
